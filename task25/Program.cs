30671001706e9e344ff0f230258f9de3d7697f5e
MaksimS <sipatovGT@mail.ru>
2022-11-08 13:05:43 +0400

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Программа принимает числа (A и B) и возводит A в B!");
+Console.Write("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+int GetExponDigit(int numberA, int numberB){
+    int result = 1;
+    for(int count = 1; count <= numberB; count++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+int result = GetExponDigit(numberA, numberB);
+
+Console.WriteLine("Результат вычисления равен: " + result);
